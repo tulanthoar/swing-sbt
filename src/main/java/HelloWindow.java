ab753394ab755802bc6b9040615e58d561e36743
Nate <tulanthoar@gmail.com>
2015-12-26 10:50:25 -0700

    just some formatting and tinkering

--- src/main/java/HelloWindow.java
+++ src/main/java/HelloWindow.java
@@ -5,7 +5,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class HelloWindow {
-  private JFrame    frame     = new JFrame();
+  private JFrame  frame  = new JFrame();
   private boolean hasImg = false;
 
   public HelloWindow() {
@@ -16,22 +16,25 @@ public class HelloWindow {
     BasicPoint mid = new BasicPoint(500, 500);
     frame.setLocation(mid.x(), mid.y());
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    JMenuBar  menuBar   = new JMenuBar();
+    JMenuBar menuBar = new JMenuBar();
     menuBar.add(HelloMenu.apply());
     menuBar.add(ExitMenu.apply());
     menuBar.setToolTipText("This Is A Menubar");
     frame.setJMenuBar(menuBar);
   }
 
-  public static void putGreeting( JMenuItem hi ) {
-    hi.addActionListener(
-        (e) -> {JOptionPane.showMessageDialog(null, "Hello, Codenvy User!");});
+  public static void putGreeting(JMenuItem hi) {
+    hi.addActionListener((e) -> {
+      JOptionPane.showMessageDialog(null, "Hello, Codenvy User!");
+    });
   }
-  
-  public static void putExit( JMenuItem bye ) {
-    bye.addActionListener( (e) -> {System.exit(0);} );
+
+  public static void putExit(JMenuItem bye) {
+    bye.addActionListener((e) -> {
+      System.exit(0);
+    });
   }
-  
+
   public void setImg(Image img) {
     JLabel label = new JLabel(new ImageIcon(img));
     frame.getContentPane().add(label);
@@ -41,7 +44,7 @@ public class HelloWindow {
   public void findImg(String address) {
     // Use image from URL
     try {
-      setImg( ImageIO.read(new URL(address)));
+      setImg(ImageIO.read(new URL(address)));
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -51,7 +54,7 @@ public class HelloWindow {
     frame.setTitle(comp.getName());
 
     // get image if not gotten
-    if ( !hasImg )
+    if (!hasImg)
       findImg("https://upload.wikimedia.org/wikipedia/en/f/fa/Codenvy_Logo.jpg");
 
     // Add a new JLabel
--- src/main/scala/ExitMenu.scala
+++ src/main/scala/ExitMenu.scala
@@ -1,21 +1,22 @@
 import javax.swing._;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import scala.language.postfixOps
 
 class ExitMenu {
-  var menu: JMenu = _
+  var _: JMenu = _
 }
 object ExitMenu {
 
-  def apply(): JMenu = {
+  def apply: JMenu = {
     var menu = new JMenu( "Exit" )
     var bye = new JMenuItem( "Close" )
     HelloWindow putExit bye
     bye setToolTipText "Exit this app"
-    bye setMnemonic KeyEvent.VK_Q
-    bye setAccelerator KeyStroke.getKeyStroke ( KeyEvent.VK_Q, ActionEvent.CTRL_MASK );
+    bye setMnemonic ( KeyEvent VK_Q )
+    bye setAccelerator ( KeyStroke getKeyStroke ( KeyEvent VK_Q, ActionEvent CTRL_MASK ) )
     menu add bye
-    return menu
+    menu
   }
 
 }
